a4e44e842e364749189690cb6bc85138e27d6ed9
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-19 14:19:17 +0100

    Refactor test

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,11 +1,10 @@
 feature 'so that I can save a website' do
   scenario "add site's title and address to bookmark manager" do
-    visit('/links/new')
+    visit('/links')
+    click_button("Add Link")
     fill_in 'url', with: 'http://www.developermemes.com/'
     fill_in 'title', with: 'funny memes'
     click_button('Add')
-    expect(current_path).to eq '/links'
-
     within 'ul#links' do
       expect(page).to have_content('memes')
     end
