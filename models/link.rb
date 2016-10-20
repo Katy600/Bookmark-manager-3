ed50df362c01b48e6708c693a4602016f3ec6d99
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-20 16:57:22 +0100

    Change order in Link class

--- models/link.rb
+++ models/link.rb
@@ -1,12 +1,11 @@
 require_relative 'data_mapper_setup'
 
 class Link
-include DataMapper::Resource
+  include DataMapper::Resource
 
-property :id, Serial
-property :title, String
-property :url, String
+  has n, :tag, through: Resource
 
-has n, :tag, through: Resource
-
+  property :id, Serial
+  property :title, String
+  property :url, String
 end
