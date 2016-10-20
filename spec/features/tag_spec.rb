52cdf6edf19bdd9072bb3b74192799ba8be2e0c4
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-20 15:06:33 +0100

    Add tag test - incomplete

---
+++ spec/features/tag_spec.rb
@@ -0,0 +1,9 @@
+feature 'So I can organise my links into different catagories'do
+  scenario 'to tag links in my bookmark manager' do 
+    visit('/links')
+    click_button("Add Link")
+    ill_in 'url', with: 'http://www.developermemes.com/'
+    fill_in 'title', with: 'funny memes'
+    fill_in 'tags', with: 'funny'
+
+    click_button('Add')
